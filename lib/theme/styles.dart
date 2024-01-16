53810d9afeec6c494b91ddccf4fa3e6bd7c5c987
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-01-16 12:43:16 +0300

    Text styles for app texts
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

---
+++ lib/theme/styles.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class TextStyles {
+  //* Primary
+  static const TextStyle pacifico30PrimaryBold = TextStyle(
+    fontSize: 30,
+    fontFamily: 'Pacifico',
+    fontWeight: FontWeight.w700,
+    color: AppColors.primaryColor,
+  );
+  static const TextStyle font18PrimaryRegular = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryColor,
+  );
+  static const TextStyle font16PrimaryRegular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryColor,
+  );
+  static const TextStyle pacifico14PrimaryRegular = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Pacifico',
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryColor,
+  );
+  static const TextStyle font14PrimaryRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryColor,
+  );
+
+  //* Second Primary
+  // static const TextStyle font20SecondPrimaryRegular = TextStyle(
+  //   fontSize: 20,
+  //   fontWeight: FontWeight.w400,
+  //   color: AppColors.secondPrimaryColor,
+  // );
+  // static const TextStyle font18SecondPrimaryRegular = TextStyle(
+  //   fontSize: 18,
+  //   fontWeight: FontWeight.w400,
+  //   color: AppColors.secondPrimaryColor,
+  // );
+  static const TextStyle font15SecondPrimaryRegular = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.secondPrimaryColor,
+  );
+  static const TextStyle font14SecondPrimaryRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.secondPrimaryColor,
+  );
+  static const TextStyle font14SecondPrimaryRegularUnderline = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.secondPrimaryColor,
+    decoration: TextDecoration.underline,
+  );
+  static const TextStyle font12SecondPrimaryRegular = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.secondPrimaryColor,
+  );
+  static const TextStyle pacifico16SecondPrimaryRegular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Pacifico',
+    color: AppColors.secondPrimaryColor,
+  );
+  static const TextStyle font15SecondPrimaryBold = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColors.secondPrimaryColor,
+  );
+
+  //* Light Primary
+  static const TextStyle font14LightPrimaryRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.lightPrimaryColor,
+  );
+  static const TextStyle pacifico15LightPrimaryRegular = TextStyle(
+    fontSize: 15,
+    fontFamily: 'Pacifico',
+    fontWeight: FontWeight.w400,
+    color: AppColors.lightPrimaryColor,
+  );
+
+  //* White
+  static const TextStyle font12WhiteRegular = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor,
+  );
+  // static const TextStyle font14WhiteRegular = TextStyle(
+  //   fontSize: 14,
+  //   fontWeight: FontWeight.w400,
+  //   color: AppColors.whiteColor,
+  // );
+  static const TextStyle pacifico14WhiteRegular = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Pacifico',
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor,
+  );
+  // static const TextStyle roboto15WhiteRegular = TextStyle(
+  //   fontSize: 15,
+  //   fontFamily: 'Roboto',
+  //   fontWeight: FontWeight.w400,
+  //   color: AppColors.whiteColor,
+  // );
+  static const TextStyle pacifico16WhiteRegular = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Pacifico',
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor,
+  );
+  static const TextStyle font16WhiteRegular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor,
+  );
+  static const TextStyle font16WhiteBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: AppColors.whiteColor,
+  );
+  static const TextStyle font16WhiteRegularHeight1_2 = TextStyle(
+    fontSize: 16,
+    height: 1.2,
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor,
+  );
+  static const TextStyle font17WhiteRegular = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor,
+  );
+  static const TextStyle pacifico17WhiteRegularLS0_5 = TextStyle(
+    fontSize: 17,
+    letterSpacing: 0.5,
+    fontFamily: 'Pacifico',
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor,
+  );
+  static const TextStyle pacifico55WhiteRegular = TextStyle(
+    fontSize: 55,
+    fontFamily: 'Pacifico',
+    fontWeight: FontWeight.w400,
+    color: AppColors.whiteColor,
+  );
+
+  //* Gray
+  static const TextStyle pacifico15GrayRegular = TextStyle(
+    fontSize: 15,
+    fontFamily: 'Pacifico',
+    fontWeight: FontWeight.w400,
+    color: AppColors.grayColor,
+  );
+
+  //* Light Gray
+  static const TextStyle font12LightGrayRegularHeight1_5 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    color: AppColors.lightGrayColor,
+  );
+
+  //* Yallow
+  static const TextStyle pacifico12YallowRegular = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Pacifico',
+    color: AppColors.yallowColor,
+  );
+  static const TextStyle roboto14YallowRegularLS0_6 = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Roboto',
+    letterSpacing: 0.6,
+    fontWeight: FontWeight.w400,
+    color: AppColors.yallowColor,
+  );
+  static const TextStyle font14YallowRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.yallowColor,
+  );
+  static const TextStyle pacifico17YallowRegular = TextStyle(
+    fontSize: 17,
+    fontFamily: 'Pacifico',
+    fontWeight: FontWeight.w400,
+    color: AppColors.yallowColor,
+  );
+}

b78310df6d6413ce0c3b0757cc4218487cd1b4bc
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-01-16 12:05:32 +0300

    For database columns names & app images
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

---
+++ lib/app/constants/strings.dart
@@ -0,0 +1,17 @@
+// *** Database Columns Names *** //
+const int databaseVersion = 1;
+const String databaseName = "cards.db";
+const String cardsTable = "cards";
+const String colId = "id";
+const String colProductId = "productId";
+const String colTitle = "title";
+const String colDesc = "desc";
+const String colPrice = "price";
+const String colCategory = "category";
+const String colImage = "image";
+const String colQuantity = "quantity";
+const String colDataTime = "dataTime";
+
+// *** App Images *** //
+const String iconAppImage = "assets/images/icon_app.png";
+const String avatarImage = "assets/images/avatar.webp";

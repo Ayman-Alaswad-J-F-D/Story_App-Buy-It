80488081649a9238121ebcdee4b1128552575d45
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-02-20 15:17:28 +0300

    Removed
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

--- lib/database/class_create_database.dart
+++
@@ -1,9 +0,0 @@
-import 'package:store_app/database/reposetory/local_database_reposetory.dart';
-import 'package:store_app/database/reposetory/local_database_reposetory_impl.dart';
-
-mixin ExecuteCreateDB {
-  final LocalDatabaseReposetory _reposetory =
-      const LocalDatabaseReposetoryImpl();
-
-  Future<void> executeCreateLocalDatabase() async => _reposetory.initDB();
-}
--- lib/database/class_delete_records_database.dart
+++
@@ -1,9 +0,0 @@
-import 'package:store_app/database/reposetory/local_database_reposetory.dart';
-import 'package:store_app/database/reposetory/local_database_reposetory_impl.dart';
-
-mixin ExecuteDeleteRecordsDB {
-  final LocalDatabaseReposetory _reposetory =
-      const LocalDatabaseReposetoryImpl();
-
-  Future<void> executeDeleteAllRecords() => _reposetory.deleteRecords();
-}
